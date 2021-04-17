6a89a6efabcca96619b06970e929fd896e34228d
SorrowOfMind <sortoflifecraze@gmail.com>
2021-04-17 14:28:54 +0200

    main categories menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,8 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $categories = $this->getDoctrine()->getRepository(Categories::class)->findAll();
+
+        return $this->render('home/index.html.twig', ['categories' => $categories]);
     }
 }
